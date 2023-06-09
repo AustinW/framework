30015b4bcde0eb538c122a8363122a02497afb9e
Austin White <austingym@gmail.com>
2023-06-08 21:26:27 -0700

    Add Arr methods to support csv/array conversions.

--- tests/Support/SupportArrTest.php
+++ tests/Support/SupportArrTest.php
@@ -1309,4 +1309,44 @@ class SupportArrTest extends TestCase
             ],
         ], Arr::select($array, 'name'));
     }
+
+    public function testFromCsv()
+    {
+        $csvString = 'apple, banana, orange';
+        $expectedArray = ['apple', 'banana', 'orange'];
+
+        $result = Arr::fromCsv($csvString);
+
+        $this->assertEquals($expectedArray, $result);
+    }
+
+    public function testFromCsvWithCustomDelimiter()
+    {
+        $csvString = 'apple;banana;orange';
+        $expectedArray = ['apple', 'banana', 'orange'];
+
+        $result = Arr::fromCsv($csvString, ';');
+
+        $this->assertEquals($expectedArray, $result);
+    }
+
+    public function testToCsv()
+    {
+        $array = ['apple', 'banana', 'orange'];
+        $expectedCsvString = 'apple,banana,orange';
+
+        $result = Arr::toCsv($array);
+
+        $this->assertEquals($expectedCsvString, $result);
+    }
+
+    public function testToCsvWithCustomDelimiter()
+    {
+        $array = ['apple', 'banana', 'orange'];
+        $expectedCsvString = 'apple;banana;orange';
+
+        $result = Arr::toCsv($array, ';');
+
+        $this->assertEquals($expectedCsvString, $result);
+    }
 }
